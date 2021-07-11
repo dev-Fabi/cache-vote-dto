b2e6e322537ef7ca7534866f0186c8af89b005f5
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-07-11 14:12:02 +0200

    1.0.0

--- src/raiting/UpdateRatingRequest.dto.ts
+++ src/raiting/UpdateRatingRequest.dto.ts
@@ -1,12 +1,12 @@
-import {IsInt, IsString, Matches, Max, Min} from "class-validator";
+import { IsInt, IsString, Matches, Max, Min } from 'class-validator';
 
 export class UpdateRatingRequestDto {
-    @IsString()
-    @Matches(/GC.{3,5}/)
-    cacheId: string;
+  @IsString()
+  @Matches(/GC.{3,5}/)
+  cacheId: string;
 
-    @IsInt()
-    @Max(5)
-    @Min(1)
-    rating: number;
+  @IsInt()
+  @Max(5)
+  @Min(1)
+  rating: number;
 }
